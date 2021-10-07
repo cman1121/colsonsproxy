a499113e6afefa7ce20ef6ac086ab0af803567a0
Daddy Deez <89707705+cman1121@users.noreply.github.com>
2021-10-07 09:56:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function go_to_proxy(){
 	var url = document.getElementById('URL').value;
-	window.location.href="https://nodeJS.taylorliang.repl.co/proxy/" + url
+	window.location.href="https://eirik.nl/proxy.php" + url
 	return false
 }
 
